75fddd830a09cc9694620dc8628019fac3b1538e
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-22 17:38:09 +0530

    fix build error

--- app/api/scores/route.ts
+++ app/api/scores/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
-import data from '../../../../sample_data.json';
+import { NextResponse } from "next/server";
+import data from "../../../sample_data.json";
 
 export async function GET(_req: Request) {
   return NextResponse.json({ data });

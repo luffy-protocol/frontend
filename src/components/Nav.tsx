edf395b007f9592cb6d64e8f43881537a42e11af
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-05-05 15:36:35 +0530

    fast psu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -99,12 +99,7 @@ function Nav() {
                   ))}
                 </div>
                 <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
+                  <DynamicWidget />
                 </div>
               </div>
             </div>

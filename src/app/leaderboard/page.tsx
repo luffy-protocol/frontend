e408891ffc62e006e65d09618bb01ec6344a0104
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-05-06 14:03:13 +0530

    weird ass errors

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -4,9 +4,7 @@ import { DynamicWidget, useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import axios from "axios";
 import request, { gql } from "graphql-request";
 import { useEffect, useState } from "react";
-import { Pixelify_Sans } from "next/font/google";
 
-const pxsans = Pixelify_Sans({ subsets: ["latin"] });
 interface User {
   id: string;
   name: string;
@@ -27,46 +25,47 @@ interface UserData {
   totalPointsWon: number;
 }
 
-const fetchAllUsers = async ({
-  mappedUsers,
-}: FetchInput): Promise<UserData[]> => {
-  try {
-    const data = await request(
-      "https://api.studio.thegraph.com/query/30735/luffy/version/latest",
-      gql`
-        query MyQuery {
-          users(orderBy: totalPointsWon, orderDirection: desc) {
-            address
-            totalGamesPlayed
-            totalGamesClaimed
-            totalPointsWon
-          }
-        }
-      `
-    );
-
-    // Extract ongoing matches from the data and set in state
-    console.log(data);
-    console.log("Mapped Users");
-    console.log(mappedUsers);
-    return (data as any).users
-      .map((user: any) => {
-        console.log("Mapped User");
-        console.log(mappedUsers[user.address]);
-        if (mappedUsers[user.address.toLowerCase()] === undefined) {
-          return null;
-        } else return { ...user, name: mappedUsers[user.address].name };
-      })
-      .filter((user: any) => user !== null);
-    // setongoing(ongoingMatchesData);
-  } catch (error) {
-    console.error("Error fetching ongoing fixtures:", error);
-    return [];
-  }
-};
 function Page() {
   const { isAuthenticated } = useDynamicContext();
   const [users, setUsers] = useState<UserData[]>([]);
+
+  const fetchAllUsers = async ({
+    mappedUsers,
+  }: FetchInput): Promise<UserData[]> => {
+    try {
+      const data = await request(
+        "https://api.studio.thegraph.com/query/30735/luffy/version/latest",
+        gql`
+          query MyQuery {
+            users(orderBy: totalPointsWon, orderDirection: desc) {
+              address
+              totalGamesPlayed
+              totalGamesClaimed
+              totalPointsWon
+            }
+          }
+        `
+      );
+
+      // Extract ongoing matches from the data and set in state
+      console.log(data);
+      console.log("Mapped Users");
+      console.log(mappedUsers);
+      return (data as any).users
+        .map((user: any) => {
+          console.log("Mapped User");
+          console.log(mappedUsers[user.address]);
+          if (mappedUsers[user.address.toLowerCase()] === undefined) {
+            return null;
+          } else return { ...user, name: mappedUsers[user.address].name };
+        })
+        .filter((user: any) => user !== null);
+      // setongoing(ongoingMatchesData);
+    } catch (error) {
+      console.error("Error fetching ongoing fixtures:", error);
+      return [];
+    }
+  };
   useEffect(() => {
     console.log(process.env.NEXT_PUBLIC_DYNAMIC_API_KEY);
     (async function () {
@@ -105,7 +104,7 @@ function Page() {
             sure you claimed the points for your predictions in the fixtures
             page.
           </p>
-          <OverallLeaderboard users={users} />
+          {/* <OverallLeaderboard users={users} /> */}
         </div>
       </div>
     </div>

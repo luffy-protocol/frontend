0b41bd8910b8f4b10241dcb845ba4f42bb310854
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-05-15 23:45:21 +0530

    Fetching Fixtures from supabase

---
+++ src/utils/supabase/fixtureHelpers/FetchFixtures.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js";
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export default async function fetchFixtures(): Promise<{
+  message: string;
+  response: any;
+}> {
+  try {
+    const { data: fixture, error } = await supabase
+      .from("fixtures")
+      .select("*");
+    if (error) throw new Error(error.message);
+
+    return { message: "Success", response: fixture };
+  } catch (error) {
+    console.error("Error fetching fixtures:", error);
+    return { message: "Internal Server Error", response: null };
+  }
+}

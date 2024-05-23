86f6f788c0f6e6933a20c558d85f47b5eb2d6400
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-05-24 00:52:49 +0530

    add constants

---
+++ src/app/placebet/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      Game Page
+    </div>
+  );
+};
+
+export default page;

8ade969e38b68b779a617a49f0147669804b481d
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-05-24 15:21:34 +0530

    randomness toggle

--- src/app/fixture/page.tsx
+++ src/app/fixture/page.tsx
@@ -258,7 +258,18 @@ function Page() {
             <img src="/assets/gas.png" alt="chain" className=" -mt-1" />
             <p className="text-[10px]  font-stalinist ">30 gwei</p>
           </div>
-          <div className="mt-60 ml-52">
+          <div className="mt-48 ml-32">
+            <div>
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" className="sr-only peer" />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800  peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border  after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#410C5E]"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 font-stalinist">
+                  Randomness
+                </span>
+              </label>
+            </div>
+          </div>
+          <div className="mt-8 ml-52">
             <div
               className={`flex items-center justify-center bg-no-repeat bg-contain w-[210%] h-full `}
               style={{

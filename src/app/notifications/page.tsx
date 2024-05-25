beceb28fac0eccb191670494a68436362a22c63c
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-05-25 20:18:02 +0530

    Notification page

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -3,6 +3,7 @@
 import getFollowingNotifications from "@/utils/notificationHelpers/notifyFollowing";
 import removeNotifications from "@/utils/notificationHelpers/removeNotification";
 import React, { useEffect, useState } from "react";
+import Navbar from "@/components/Navbar";
 
 interface Notification {
   id: number;
@@ -29,14 +30,34 @@ const NotificationCard: React.FC<NotificationCardProps> = ({
   onClose,
 }) => {
   return (
-    <div className="border p-4 mb-2 flex justify-between items-center bg-white shadow-md text-black font-stalinist">
-      <div>{`User ${notification.followingId} started following you.`}</div>
-      <button
-        className="bg-red-500 text-white px-3 py-1 rounded"
-        onClick={() => onClose(notification.id)}
-      >
-        Close
-      </button>
+    <div
+      className={`flex items-center justify-evenly  bg-no-repeat bg-contain  bg-center w-full h-[80px] xl:h-[100px]`}
+      style={{
+        backgroundImage: `url('/assets/Notification.png')`,
+      }}
+    >
+      <div className="flex gap-20">
+        <div className=" font-stalinist text-xl flex whitespace-nowrap gap-1 items-center">
+          {/* {`User ${notification.followingId} started following you.`} */}
+          <div>{"User"} </div>
+          <div className="text-[#D8485F] ">{notification.followingId}</div>
+          <div> {" started following you."}</div>
+        </div>
+        {/* <button
+          className="bg-red-500 text-white px-3 py-1 rounded"
+          onClick={() => onClose(notification.id)}
+        >
+          Close
+        </button> */}
+        <div
+          className={`flex justify-center items-center bg-contain bg-no-repeat h-[40px] xl:h-[60px]`}
+          style={{
+            backgroundImage: `url('/assets/close.png')`,
+            width: "100px",
+          }}
+          onClick={() => onClose(notification.id)}
+        ></div>
+      </div>
     </div>
   );
 };
@@ -62,16 +83,23 @@ const Page: React.FC<PageProps> = ({ params }) => {
   };
 
   return (
-    <div className="p-5">
-      <div className="text-2xl font-bold mb-4">Notifications</div>
-      <div>
-        {notifications.map((notification) => (
-          <NotificationCard
-            key={notification.id}
-            notification={notification}
-            onClose={handleCloseNotification}
-          />
-        ))}
+    <div className="flex flex-col px-10 items-center bg-no-repeat w-full h-[1350px] overflow-hidden xl:h-[1800px] bg-[url('/assets/BG.svg')]  bg-contain">
+      <div className="w-full">
+        <Navbar />
+      </div>
+      <div className="flex flex-col justify-center items-center w-full gap-20">
+        <div className=" font-stalinist  text-2xl xl:text-4xl text-[#D8485F] self-center">
+          Notifications
+        </div>
+        <div className="flex flex-col gap-6 w-full">
+          {notifications.map((notification) => (
+            <NotificationCard
+              key={notification.id}
+              notification={notification}
+              onClose={handleCloseNotification}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );

0524644c89e99a78c849cd0de3451884ce1ce5ae
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-05-27 15:38:18 +0530

    fix log in button style

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 const LoginButton = () => {
   return (
-    <div
-      className={`flex items-center justify-center bg-no-repeat bg-contain w-[210%] h-full`}
-      style={{
-        backgroundImage: `url('/assets/LoginBorder.svg')`,
-      }}
-    >
-      <span className="text-sm font-stalinist flex justify-center self-center px-3 py-2 ">
-        Login
-      </span>
+    <div className="w-[140px]">
+      <button
+        className="button-with-svg"
+        style={{
+          backgroundImage: 'url("/assets/LoginBorder.svg")',
+          backgroundSize: "cover",
+          width: "140px",
+          height: "54px",
+        }}
+      >
+        <span className="text-sm font-stalinist flex justify-center items-center self-center px-3 py-2 ">
+          Login
+        </span>
+      </button>
     </div>
   );
 };

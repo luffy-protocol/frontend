e40918becdc0b86d22e772401da04a1c0215c08a
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-05-27 11:25:12 +0530

    landing BG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@ import React from "react";
 
 function Page() {
   return (
-    <div className="flex flex-col px-10 items-center bg-no-repeat h-[800px] xl:h-[1050px] overflow-hidden bg-[url('/assets/Landing.png')] bg-contain">
-      <div className="w-full xl:mt-20">
-        <Navbar />
-        <img src="/assets/luffy.svg" className="w-1/2 mx-auto" />
+    <div className="">
+      <div className=" relative z-10 mx-2">
+        <img src="/assets/Landing.png" className=" w-screen" />
+      </div>
+      <div className="absolute inset-0 z-20 ">
+        <div className="flex flex-col px-10 items-center bg-no-repeat w-full overflow-hidden  bg-contain font-stalinist">
+          <div className="w-full xl:mt-20">
+            <Navbar />
+            <img src="/assets/luffy.svg" className="w-1/2 mx-auto" />
+          </div>
+        </div>
       </div>
     </div>
   );

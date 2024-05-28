6768456055f0cecee00d48428d900d4f2ca1ec67
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-05-28 17:09:59 +0530

    minor error

--- src/utils/resolveTokens.ts
+++ src/utils/resolveTokens.ts
@@ -1,7 +1,7 @@
 import { Option } from "./interface";
 
 export default function resolveTokens(elements: any, chain: number): Option[] {
-  if (chain == 1) {
+  if (chain == 0) {
     const tokens = elements.tokens;
     tokens[0].image = elements.chains[0].image;
     return tokens;

518ff4d1b8e87bee5a24ce5db36ee495434b9597
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-05-29 23:54:09 +0530

    ladingFix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ function Page() {
       <div className="relative z-10">
         <img src="/assets/Landing.png" className="h-screen w-screen" />
       </div>
-      <div className="absolute inset-0 z-20 mt-10">
+      <div className="absolute inset-0 z-20 mt-24">
         <div className="flex flex-col px-10 justify-center  items-center bg-no-repeat w-full overflow-hidden  bg-contain font-stalinist">
           <div className="w-full xl:mt-20">
             <img src="/assets/Luffy.svg" className="w-1/2 mx-auto" />

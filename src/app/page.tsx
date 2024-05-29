a863cf66bf7c7a666b544118e1c542762f659877
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-05-30 00:06:46 +0530

    landing Againnnnnn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,21 @@ function Page() {
       <div className="relative z-10">
         <img src="/assets/Landing.png" className="h-screen w-screen" />
       </div>
-      <div className="absolute inset-0 z-20 mt-24">
-        <div className="flex flex-col px-10 justify-center  items-center bg-no-repeat w-full overflow-hidden  bg-contain font-stalinist">
-          <div className="w-full xl:mt-20">
-            <img src="/assets/Luffy.svg" className="w-1/2 mx-auto" />
+      <div className="absolute inset-0 z-20">
+        <div className=" flex justify-between flex-col h-screen">
+          <div className="flex flex-col px-10 justify-center  items-center bg-no-repeat w-full overflow-hidden  bg-contain font-stalinist mt-32">
+            <div className="w-full xl:mt-20">
+              <img src="/assets/Luffy.svg" className="w-1/2 mx-auto" />
+            </div>
+            <div className="mt-12 hover:scale-110">
+              <LoginButton />
+            </div>
+            <div className="scale-50 w-[1700px] mt-10">
+              <Slider />
+            </div>
           </div>
-          <div className="mt-12 hover:scale-110">
-            <LoginButton />
-          </div>
-          <div className="scale-50 w-[1700px] -mt-10">
-            <Slider />
-            <div className=" mt-12 scale-90 pt-1">
+          <div>
+            <div className="scale-50">
               <Socials />
             </div>
           </div>

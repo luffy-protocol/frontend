ff1bde0fe74a370dfdc14e00aec0968bdd3acf3f
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-06-01 17:57:28 +0530

    steps

--- src/components/Battery.tsx
+++ src/components/Battery.tsx
@@ -6,6 +6,7 @@ interface BatteryProps {
 }
 
 const Battery: React.FC<BatteryProps> = ({ step, totalSteps }) => {
+  const steppx = ["240", "110", "70", "50"];
   useEffect(() => {}, [step]);
   return (
     <div className="flex bg-[url('/assets/battery.svg')] sm:w-[150px] h-[530px] bg-no-repeat bg-cover items-center justify-start flex-col-reverse gap-2">
@@ -14,9 +15,9 @@ const Battery: React.FC<BatteryProps> = ({ step, totalSteps }) => {
         .map((_, index) => (
           <div
             key={index}
-            className={`w-[105px] ${
-              totalSteps == 4 ? "h-[50px]" : "h-[70px] mb-2"
-            } rounded-sm bg-purple-600 shadow-2xl shadow-purple-700 ${
+            className={`w-[105px] ${`h-[${
+              steppx[totalSteps - 1]
+            }px]`} mb-2 rounded-sm bg-purple-600 shadow-2xl shadow-purple-700 ${
               index < step * 2
                 ? index == step * 2 - 2 || index == step * 2 - 1
                   ? "animate-fill-drain"
